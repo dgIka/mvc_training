61aed0acdc7616e0b9b9624e5dae4b4a629d090c
Ika <rexar.ika@gmail.com>
2025-04-18 15:56:35 +0300

    GET parameters

--- Spring_mvc_training/src/main/java/ika/mvctraining/controllers/FirstController.java
+++ Spring_mvc_training/src/main/java/ika/mvctraining/controllers/FirstController.java
@@ -1,14 +1,20 @@
 package ika.mvctraining.controllers;
 
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", defaultValue = "false") String name,         // @RequestParam("name", requierd = false)  позволит передавать Get запрос без параметров, иначе выдаст ошибку
+                            @RequestParam(value = "surname", defaultValue = "false") String surname) {
+        System.out.println("Hello " + name + " " + surname);
+
         return "first/hello";
     }
 
