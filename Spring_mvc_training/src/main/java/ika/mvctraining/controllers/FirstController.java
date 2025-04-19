21447060fe1554a7f16a1f880d64517cb3a68acb
Ika <rexar.ika@gmail.com>
2025-04-20 02:29:58 +0300

    some practice

--- Spring_mvc_training/src/main/java/ika/mvctraining/controllers/FirstController.java
+++ Spring_mvc_training/src/main/java/ika/mvctraining/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ika.mvctraining.controllers;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,15 +12,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", defaultValue = "false") String name,         // @RequestParam("name", requierd = false)  позволит передавать Get запрос без параметров, иначе выдаст ошибку
-                            @RequestParam(value = "surname", defaultValue = "false") String surname) {
-        System.out.println("Hello " + name + " " + surname);
-
+    public String helloPage(@RequestParam(value = "name", defaultValue = "nothing") String name,         // @RequestParam("name", requierd = false)  позволит передавать Get запрос без параметров, иначе выдаст ошибку
+                            @RequestParam(value = "surname", defaultValue = "happened") String surname,
+                            Model model) {
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+
+    @RequestMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", defaultValue = "0") String a,
+                             @RequestParam(value = "b", defaultValue = "1")String b,
+                             @RequestParam(value = "action", defaultValue = "0")String action, Model model) {
+        double result;
+        int d = Integer.parseInt(a);
+        int e = Integer.parseInt(b);
+        switch (action) {
+            case "multiplication": result = d * e;
+            break;
+            case "addition": result = d + e;
+            break;
+            case "subtraction": result = d - e;
+            break;
+            case "division" : result = (double) d / e;
+            break;
+            default: result = 0;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
